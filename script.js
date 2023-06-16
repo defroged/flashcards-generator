78d4d87799bf0c6907333fc647bed7b3c562b2ad
Ron Ward <defroged@gmail.com>
2023-06-17 08:54:29 +0900

    Moved files to root

---
+++ script.js
@@ -0,0 +1,21 @@
+document.getElementById('image-form').addEventListener('submit', async (event) => {
+    event.preventDefault();
+    
+    const flashcardText = document.getElementById('flashcard-text').value;
+    const imagePrompt = document.getElementById('image-prompt').value;
+
+    const response = await fetch('/.netlify/functions/generateImage', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ prompt: imagePrompt })
+    });
+    const data = await response.json();
+
+    const img = document.createElement('img');
+    img.src = data.url;
+    img.alt = flashcardText;
+
+    document.getElementById('image-container').appendChild(img);
+});
