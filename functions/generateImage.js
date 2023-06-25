66d3bbeadd21744e3c82ff3ca1393c9f51f4b4b9
Ron Ward <defroged@gmail.com>
2023-06-25 21:30:12 +0900

    adding error logs to generateImage.js file

--- functions/generateImage.js
+++ functions/generateImage.js
@@ -6,33 +6,42 @@ exports.handler = async function(event, context) {
     }
 
     const { prompt } = JSON.parse(event.body);
-    const response = await fetch('https://api.openai.com/v1/images/generations', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
-        },
-        body: JSON.stringify({
-            'prompt': prompt + " clipart",
-            'n': 1,
-            'size': '512x512'
-        })
+    try {
+        const response = await fetch('https://api.openai.com/v1/images/generations', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+            },
+            body: JSON.stringify({
+                'prompt': prompt + " clipart",
+                'n': 1,
+                'size': '512x512'
+            })
 
-    });
-    const data = await response.json();
+        });
+        const data = await response.json();
+        console.log('API response: ', data);
 
-    if (data.choices && data.choices.length > 0) {
-        // Extract the base64-encoded image from the API response
-        const base64Image = data.choices[0].image;
+        if (data.choices && data.choices.length > 0) {
+            // Extract the base64-encoded image from the API response
+            const base64Image = data.choices[0].image;
+
+            return {
+                statusCode: 200,
+                body: JSON.stringify({ imageUrl: `data:image/png;base64,${base64Image}` })
+            };
+        }
 
         return {
-            statusCode: 200,
-            body: JSON.stringify({ imageUrl: `data:image/png;base64,${base64Image}` })
+            statusCode: 500,
+            body: "Image generation failed"
+        };
+    } catch (error) {
+        console.error('Error: ', error);
+        return {
+            statusCode: 500,
+            body: `Error: ${error.message}`
         };
     }
-
-    return {
-        statusCode: 500,
-        body: "Image generation failed"
-    };
 };
