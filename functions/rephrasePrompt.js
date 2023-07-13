d77037eafdaec0809803f81b1b6b599d202252a4
Ron Ward <defroged@gmail.com>
2023-07-13 22:52:40 +0900

    prompt rephrase 102

--- functions/rephrasePrompt.js
+++ functions/rephrasePrompt.js
@@ -19,11 +19,11 @@ exports.handler = async function(event, context) {
             },
             body: JSON.stringify({
     model: 'text-davinci-002',
-    prompt: `add to the following AI image generator prompt more words to help describe the image better and in more details. Also add the words "cute" and "in simple color pencil style": \n\n"${userPrompt}"\n\n`,
+    prompt: `add to the following AI image generator prompt more words to help create a more precise image. Also add the words "cute" and "in simple color pencil style": \n\n"${userPrompt}"\n\n`,
     max_tokens: 50,
     n: 1,
     stop: null,
-    temperature: 0.8
+    temperature: 0.6
 })
 
 
